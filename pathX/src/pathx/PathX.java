9a734f999a48cc1aa49c6d7d27da78c67ab6d9a7
Jessica <jessicazengg@gmail.com>
2014-04-09 20:44:21 -0400

    Started game in main

--- pathX/src/pathx/PathX.java
+++ pathX/src/pathx/PathX.java
@@ -1,24 +1,29 @@
-import properties_manager.PropertiesManager;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package pathx;
+
+import pathx.ui.pathXGame;
+import properties_manager.PropertiesManager;
+import xml_utilities.InvalidXMLFileFormatException;
 
 /**
  *
  * @author Jessica
  */
 public class PathX {
+    // THIS HAS THE FULL USER INTERFACE AND ONCE IN EVENT
+    // HANDLING MODE, BASICALLY IT BECOMES THE FOCAL
+    // POINT, RUNNING THE UI AND EVERYTHING ELSE
+    static pathXGame game = new pathXGame();
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // NOW WE CAN LOAD THE UI, WHICH WILL USE ALL THE FLAVORED CONTENT
+        //String appTitle = props.getProperty(SortingHatPropertyType.TEXT_TITLE_BAR_GAME);
+        game.initMiniGame("pathX", 30, 750, 650);
+            
+        // GET THE PROPER WINDOW DIMENSIONS
+        game.startGame();
     }
     
 }

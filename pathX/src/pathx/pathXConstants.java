25015a9c954e445d1f6c37d1dcaeec5ba0b3e762
Jessica <jessicazengg@gmail.com>
2014-04-10 21:52:34 -0400

    added xml and properties

---
+++ pathX/src/pathx/pathXConstants.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pathx;
+
+import java.awt.Color;
+import java.awt.Font;
+
+/**
+ *
+ * @author Jessica
+ */
+public class pathXConstants {
+    // WE NEED THESE CONSTANTS JUST TO GET STARTED
+    // LOADING SETTINGS FROM OUR XML FILES
+    public static String PROPERTY_TYPES_LIST = "property_types.txt";
+    public static String PROPERTIES_FILE_NAME = "properties.xml";
+    public static String PROPERTIES_SCHEMA_FILE_NAME = "properties_schema.xsd";    
+    public static String PATH_DATA = "./data/";
+    
+    public static final int WINDOW_WIDTH = 750;
+    public static final int WINDOW_HEIGHT = 650;
+}

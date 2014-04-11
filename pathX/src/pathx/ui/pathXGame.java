b47bc39e51acbba88a653c945e7a35ff069b0a8b
Jessica <jessicazengg@gmail.com>
2014-04-11 17:33:53 -0400

    Added variables to the top

--- pathX/src/pathx/ui/pathXGame.java
+++ pathX/src/pathx/ui/pathXGame.java
@@ -5,12 +5,26 @@ import mini_game.MiniGameState;
 import mini_game.Sprite;
 import mini_game.SpriteType;
 import mini_game.Viewport;
+import pathx.file.pathXFileManager;
+import pathx.data.Record;
 
 /**
  *
  * @author Jessica
  */
 public class pathXGame  extends MiniGame{
+    // THE PLAYER RECORD FOR EACH LEVEL, WHICH LIVES BEYOND ONE SESSION
+    private Record record;
+
+    // HANDLES GAME UI EVENTS
+    private pathXEventHandler eventHandler;
+    
+    // HANDLES ERROR CONDITIONS
+    private pathXErrorHandler errorHandler;
+    
+    // MANAGES LOADING OF LEVELS AND THE PLAYER RECORDS FILES
+    private pathXFileManager fileManager;
+    
     // THE SCREEN CURRENTLY BEING PLAYED
     private String currentScreenState;
     

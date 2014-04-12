50ecc3eabd69b1cff09b61136a3dda98a8c1b8f2
Jessica <jessicazengg@gmail.com>
2014-04-11 23:19:38 -0400

    added scrolling positions

--- pathX/src/pathx/pathXConstants.java
+++ pathX/src/pathx/pathXConstants.java
@@ -33,6 +33,10 @@ public class pathXConstants {
     public static final String SETTINGS_BUTTON_TYPE = "SETTINGS_BUTTON_TYPE";
     public static final String HELP_BUTTON_TYPE = "HELP_BUTTON_TYPE";
     public static final String CLOSE_BUTTON_TYPE = "CLOSE_BUTTON_TYPE";
+    public static final String SCROLL_DOWN_BUTTON_TYPE = "SCROLL_DOWN_BUTTON_TYPE";
+    public static final String SCROLL_UP_BUTTON_TYPE = "SCROLL_UP_BUTTON_TYPE";
+    public static final String SCROLL_LEFT_BUTTON_TYPE = "SCROLL_LEFT_BUTTON_TYPE";
+    public static final String SCROLL_RIGHT_BUTTON_TYPE = "SCROLL_RIGHT_BUTTON_TYPE";
     
     public static final int WINDOW_WIDTH = 640;
     public static final int WINDOW_HEIGHT = 480;
@@ -41,10 +45,18 @@ public class pathXConstants {
     public static final int MENU_BUTTON_Y = 385;
     public static final int CLOSE_BUTTON_X = 585;
     public static final int CLOSE_BUTTON_Y = 8;
+    public static final int DOWN_BUTTON_X = 15;
+    public static final int DOWN_BUTTON_Y = 390;
+    public static final int UP_BUTTON_X = 15;
+    public static final int UP_BUTTON_Y = 370;
+    public static final int LEFT_BUTTON_X = 8;
+    public static final int LEFT_BUTTON_Y = 360;
+    public static final int RIGHT_BUTTON_X = 22;
+    public static final int RIGHT_BUTTON_Y = 360;
     
     // WE'LL USE THESE STATES TO CONTROL SWITCHING BETWEEN THE TWO
     public static final String MENU_SCREEN_STATE = "MENU_SCREEN_STATE";
-    public static final String GAME_SCREEN_STATE = "GAME_SCREEN_STATE";  
+    public static final String LEVEL_SCREEN_STATE = "LEVEL_SCREEN_STATE";  
     
     // COLORS USED FOR RENDERING VARIOUS THINGS, INCLUDING THE
     // COLOR KEY, WHICH REFERS TO THE COLOR TO IGNORE WHEN

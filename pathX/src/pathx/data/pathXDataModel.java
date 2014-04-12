fef806d03df26e08847d32d90f3ebb82b9717d28
Jessica <jessicazengg@gmail.com>
2014-04-11 21:35:00 -0400

    Added constructor, overridden methods and API

---
+++ pathX/src/pathx/data/pathXDataModel.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pathx.data;
+
+import mini_game.MiniGame;
+import mini_game.MiniGameDataModel;
+
+/**
+ *
+ * @author Jessica
+ */
+public class pathXDataModel extends MiniGameDataModel {
+    // THIS CLASS HAS A REFERERENCE TO THE MINI GAME SO THAT IT
+    // CAN NOTIFY IT TO UPDATE THE DISPLAY WHEN THE DATA MODEL CHANGES
+    private MiniGame miniGame;
+    
+    public pathXDataModel(MiniGame initMiniGame)
+    {
+        // KEEP THE GAME FOR LATER
+        miniGame = initMiniGame;
+    }
+    
+    /**
+     * This method provides a custom game response for handling mouse clicks on
+     * the game screen. We'll use this to close game dialogs as well as to
+     * listen for mouse clicks on grid cells.
+     *
+     * @param game The Sorting Hat game.
+     *
+     * @param x The x-axis pixel location of the mouse click.
+     *
+     * @param y The y-axis pixel location of the mouse click.
+     */
+    @Override
+    public void checkMousePressOnSprites(MiniGame game, int x, int y)
+    {
+        
+    }
+    
+    /**
+     * Called when the game is won, it will record the ending game time, update
+     * the player record, display the win dialog, and play the win animation.
+     */
+    @Override
+    public void endGameAsWin(){
+        
+    }
+    
+    /**
+     * Updates the player record, adding a game without a win.
+     */
+    public void endGameAsLoss(){
+        
+    }
+    
+     /**
+     * Called when a game is started, the game grid is reset.
+     *
+     * @param game
+     */
+    @Override
+    public void reset(MiniGame game){
+        
+    }
+    
+    /**
+     * Called each frame, this method updates all the game objects.
+     *
+     * @param game The Sorting Hat game to be updated.
+     */
+    @Override
+    public void updateAll(MiniGame game){
+        
+    }
+    
+     /**
+     * This method is for updating any debug text to present to the screen. In a
+     * graphical application like this it's sometimes useful to display data in
+     * the GUI.
+     *
+     * @param game The Sorting Hat game about which to display info.
+     */
+    @Override
+    public void updateDebugText(MiniGame game){
+        
+    }
+}

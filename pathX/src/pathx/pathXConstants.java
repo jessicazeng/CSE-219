4cd4639e78e75c45aa6c435dd769c52dcb3daad6
Jessica <jessicazengg@gmail.com>
2014-04-11 21:33:29 -0400

    updated menu button positions

--- pathX/src/pathx/pathXConstants.java
+++ pathX/src/pathx/pathXConstants.java
@@ -21,6 +21,37 @@ public class pathXConstants {
     public static String PROPERTIES_SCHEMA_FILE_NAME = "properties_schema.xsd";    
     public static String PATH_DATA = "./data/";
     
-    public static final int WINDOW_WIDTH = 750;
-    public static final int WINDOW_HEIGHT = 650;
+    // THESE ARE THE TYPES OF CONTROLS, WE USE THESE CONSTANTS BECAUSE WE'LL
+    // STORE THEM BY TYPE, SO THESE WILL PROVIDE A MEANS OF IDENTIFYING THEM
+    
+    // EACH SCREEN HAS ITS OWN BACKGROUND TYPE
+    public static final String BACKGROUND_TYPE = "BACKGROUND_TYPE";
+    
+    // THIS REPRESENTS THE BUTTONS ON THE MENU SCREEN FOR LEVEL SELECTION
+    public static final String PLAY_BUTTON_TYPE = "PLAY_BUTTON_TYPE";
+    public static final String RESET_BUTTON_TYPE = "RESET_BUTTON_TYPE";
+    public static final String SETTINGS_BUTTON_TYPE = "SETTINGS_BUTTON_TYPE";
+    public static final String HELP_BUTTON_TYPE = "HELP_BUTTON_TYPE";
+    public static final String CLOSE_BUTTON_TYPE = "CLOSE_BUTTON_TYPE";
+    
+    public static final int WINDOW_WIDTH = 640;
+    public static final int WINDOW_HEIGHT = 480;
+    public static final int MENU_BUTTON_WIDTH = 100;
+    public static final int MENU_BUTTON_MARGIN = 15;
+    public static final int MENU_BUTTON_Y = 385;
+    public static final int CLOSE_BUTTON_X = 585;
+    public static final int CLOSE_BUTTON_Y = 8;
+    
+    // WE'LL USE THESE STATES TO CONTROL SWITCHING BETWEEN THE TWO
+    public static final String MENU_SCREEN_STATE = "MENU_SCREEN_STATE";
+    public static final String GAME_SCREEN_STATE = "GAME_SCREEN_STATE";  
+    
+    // COLORS USED FOR RENDERING VARIOUS THINGS, INCLUDING THE
+    // COLOR KEY, WHICH REFERS TO THE COLOR TO IGNORE WHEN
+    // LOADING ART.
+    public static final Color COLOR_KEY = new Color(255, 174, 201);
+    public static final Color COLOR_DEBUG_TEXT = Color.BLACK;
+    
+    // FONTS USED DURING FOR TEXTUAL GAME DISPLAYS
+    public static final Font FONT_DEBUG_TEXT = new Font(Font.MONOSPACED, Font.BOLD, 14);
 }

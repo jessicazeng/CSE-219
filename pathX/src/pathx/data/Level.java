926399d7992fbfa64c8e4e7c7d3d6ef87f23d617
Jessica <jessicazengg@gmail.com>
2014-04-13 21:45:38 -0400

    Added level attributes

---
+++ pathX/src/pathx/data/Level.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pathx.data;
+
+/**
+ *
+ * @author Jessica
+ */
+public class Level {
+    public String levelname;
+    public int xposition;
+    public int yposition;
+    public String state;
+    public boolean locked;
+    public boolean levelCompleted;
+}

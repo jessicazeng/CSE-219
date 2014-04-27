8ffcb9fd8855980cf2f5bc75bf0044e1cc69845c
Jessica <jessicazengg@gmail.com>
2014-04-26 22:30:03 -0400

    Can read in the background image from the binary file to put into game screen

--- pathX/src/pathx/file/pathXFileManager.java
+++ pathX/src/pathx/file/pathXFileManager.java
@@ -1,6 +1,11 @@
 package pathx.file;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import org.w3c.dom.Document;
 import javax.xml.parsers.DocumentBuilder;
@@ -11,6 +16,7 @@ import org.w3c.dom.NodeList;
 import pathx.PathX;
 import pathx.data.Record;
 import pathx.data.pathXDataModel;
+import static pathx.pathXConstants.*;
 import pathx.ui.pathXGame;
 import properties_manager.PropertiesManager;
 
@@ -45,19 +51,39 @@ public class pathXFileManager {
             ArrayList<String> levelFiles = props.getPropertyOptionsList(PathX.pathXPropertyType.LEVEL_FILES);
             //find the level
             for (int i = 0; i < levels.size(); i++){ 
-                if(levels.get(i).matches(levelFile)){
-                    String gameImage = "./pathx/californiabg.png";
+                if((levels.get(i)).matches(levelFile)){
+                    String levelFileName = PATH_DATA + levelFiles.get(i);
+                    
+                    File fileToOpen = new File(levelFileName);
+                    byte[] bytes = new byte[Long.valueOf(fileToOpen.length()).intValue()];
+                    ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+                    FileInputStream fis = new FileInputStream(fileToOpen);
+                    BufferedInputStream bis = new BufferedInputStream(fis);
+            
+                    // HERE IT IS, THE ONLY READY REQUEST WE NEED
+                    bis.read(bytes);
+                    bis.close();
+                    
+                    // NOW WE NEED TO LOAD THE DATA FROM THE BYTE ARRAY
+                    DataInputStream dis = new DataInputStream(bais);
+                    
+                    // FIRST READ THE ALGORITHM NAME TO USE FOR THE LEVEL
+                    String levelName = dis.readUTF();
+                    
+                    // THEN GET THE BACKGROUND IMAGE NAME
+                    String bgImageName = "./pathx/" + dis.readUTF();
+                    
                     
                     Record record = miniGame.getPlayerRecord();
                     
                     // set level background image
-                    record.setLevelImage(levelFile, gameImage);
+                    record.setLevelImage(levelFile, bgImageName);
                     
                     pathXDataModel dataModel = (pathXDataModel)miniGame.getDataModel();
                     dataModel.setCurrentLevel(levelFile);
                 }
             }
-        } catch(Exception e){
+        } catch(IOException e){
             
         }
     }

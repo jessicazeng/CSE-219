b4dcd85c24ed8b7c520e8fa637aaf3f3dbeb2b1f
Jessica <jessicazengg@gmail.com>
2014-04-27 00:29:34 -0400

    added nodes road is connected to, accessor and mutator methods

---
+++ pathX/src/pathx/data/Road.java
@@ -0,0 +1,29 @@
+package pathx.data;
+
+/**
+ *
+ * @author Jessica
+ */
+public class Road {
+    // THESE ARE THE EDGE'S NODES
+    Intersection node1;
+    Intersection node2;
+    
+    // false IF IT'S TWO-WAY, true IF IT'S ONE WAY
+    boolean oneWay;
+    
+    // ROAD SPEED LIMIT
+    int speedLimit;
+
+    // ACCESSOR METHODS
+    public Intersection getNode1()  {   return node1;       }
+    public Intersection getNode2()  {   return node2;       }
+    public boolean isOneWay()       {   return oneWay;      }
+    public int getSpeedLimit()      {   return speedLimit;  }
+    
+    // MUTATOR METHODS
+    public void setNode1(Intersection node1)    {   this.node1 = node1;             }
+    public void setNode2(Intersection node2)    {   this.node2 = node2;             }
+    public void setOneWay(boolean oneWay)       {   this.oneWay = oneWay;           }
+    public void setSpeedLimit(int speedLimit)   {   this.speedLimit = speedLimit;   }
+}

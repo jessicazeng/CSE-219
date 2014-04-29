18dd58f4e4d998dc84fc37aa8872135e8d6a47b1
Jessica <jessicazengg@gmail.com>
2014-04-28 22:52:13 -0400

    finished loadLevel method - reads in all data for a level and stores it in Record

--- pathX/src/pathx/file/pathXFileManager.java
+++ pathX/src/pathx/file/pathXFileManager.java
@@ -14,7 +14,9 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import pathx.PathX;
+import pathx.data.Intersection;
 import pathx.data.Record;
+import pathx.data.Road;
 import pathx.data.pathXDataModel;
 import static pathx.pathXConstants.*;
 import pathx.ui.pathXGame;
@@ -49,6 +51,8 @@ public class pathXFileManager {
             PropertiesManager props = PropertiesManager.getPropertiesManager();
             ArrayList<String> levels = props.getPropertyOptionsList(PathX.pathXPropertyType.LEVEL_OPTIONS);
             ArrayList<String> levelFiles = props.getPropertyOptionsList(PathX.pathXPropertyType.LEVEL_FILES);
+            Record record = miniGame.getPlayerRecord();
+            
             //find the level
             for (int i = 0; i < levels.size(); i++){ 
                 if((levels.get(i)).matches(levelFile)){
@@ -73,8 +77,82 @@ public class pathXFileManager {
                     // THEN GET THE BACKGROUND IMAGE NAME
                     String bgImageName = "./pathx/" + dis.readUTF();
                     
+                    // MAKE ARRAY LIST TO STORE INTERSECTIONS AS WE READ THEM IN
+                    ArrayList<Intersection> intersections = new ArrayList();
                     
-                    Record record = miniGame.getPlayerRecord();
+                    // READ IN NUMBER OF INTERSECTIONS IN THIS LEVEL
+                    int numIntersections = dis.readInt();
+                    
+                    // AND THEN GO THROUGH AND ADD ALL THE LISTED REGIONS
+                    for (int j = 0; j < numIntersections; j++){
+                        // GET THEIR DATA FROM THE DOC
+                        int x = dis.readInt();
+                        int y = dis.readInt();
+                        boolean isOpen = dis.readBoolean();
+                        
+                        // NOW MAKE AND ADD THE INTERSECTION
+                        Intersection newIntersection = new Intersection(x, y);
+                        newIntersection.setOpen(isOpen);
+                        intersections.add(newIntersection);
+                    }
+                    
+                    // add list of intersections into the level record
+                    record.addIntersections(levelFile, intersections);
+                    
+                    // make array list to store roads
+                    ArrayList<Road> roads = new ArrayList();
+                    
+                    // read in number of road
+                    int numRoads = dis.readInt();
+                    
+                    // AND THEN GO THROUGH AND ADD ALL THE LISTED ROADS
+                    for (int k = 0; k < numRoads; k++)
+                    {
+                        // GET THEIR DATA FROM THE DOC
+                        int int_id1 = dis.readInt();
+                        int int_id2 = dis.readInt();
+                        boolean oneWay = dis.readBoolean();
+                        int speedLimit = dis.readInt();
+            
+                        // NOW MAKE AND ADD THE ROAD
+                        Road newRoad = new Road();
+                        newRoad.setNode1(intersections.get(int_id1));
+                        newRoad.setNode2(intersections.get(int_id2));
+                        newRoad.setOneWay(oneWay);
+                        newRoad.setSpeedLimit(speedLimit);
+                        roads.add(newRoad);
+                    }
+                    
+                    record.addRoads(levelFile, roads);
+                    
+                    // LOAD THE START INTERSECTION
+                    int startId = dis.readInt();
+                    String startImageName = "./pathx/" + dis.readUTF();
+                    
+                    record.addStartImage(levelFile, startImageName);
+                    
+                    // LOAD THE DESTINATION
+                    int destId = dis.readInt();
+                    String destImageName = "./pathx/" + dis.readUTF();
+                    
+                    record.addEndImage(levelFile, destImageName);
+                    
+                    // LOAD THE MONEY
+                    int money = dis.readInt();
+                    
+                    record.setMoney(levelFile, money);
+                    
+                    // LOAD THE NUMBER OF POLICE
+                    int numPolice = dis.readInt();
+                    record.setNumPolice(levelFile, numPolice);
+                    
+                    // LOAD THE NUMBER OF BANDITS
+                    int numBandits = dis.readInt();
+                    record.setNumBandits(levelFile, numBandits);
+            
+                    // LOAD THE NUMBER OF ZOMBIES
+                    int numZombies = dis.readInt();
+                    record.setNumZombies(levelFile, numZombies);
                     
                     // set level background image
                     record.setLevelImage(levelFile, bgImageName);

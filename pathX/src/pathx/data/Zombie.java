4b432ffe2c4f7c585a2ea7b09af623f090d6e652
Jessica <jessicazengg@gmail.com>
2014-04-30 17:32:06 -0400

    added constructor with the node the zombie starts at

---
+++ pathX/src/pathx/data/Zombie.java
@@ -0,0 +1,17 @@
+package pathx.data;
+
+/**
+ *
+ * @author Jessica
+ */
+public class Zombie {
+    int Node;
+    
+    public Zombie(int newNode){
+        Node = newNode;
+    }
+    
+    public int getNode(){
+        return Node;
+    }
+}

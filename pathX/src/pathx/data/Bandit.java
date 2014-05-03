107c76ea9cfaba3ff87726c9349e114f538bbb13
Jessica <jessicazengg@gmail.com>
2014-05-02 20:55:22 -0400

    added constructor for sprite

--- pathX/src/pathx/data/Bandit.java
+++ pathX/src/pathx/data/Bandit.java
@@ -6,15 +6,35 @@
 
 package pathx.data;
 
+import mini_game.Sprite;
+import mini_game.SpriteType;
+
 /**
  *
  * @author Jessica
  */
-public class Bandit {
+public class Bandit extends Sprite{
+    int ID;
+    
+    // the current node this sprite is in
     int Node;
     
-    public Bandit(int newNode){
-        Node = newNode;
+    int targetX;
+    int targetY;
+    
+    int startX;
+    int startY;
+    
+    // THIS IS true WHEN THIS TILE IS MOVING, WHICH HELPS US FIGURE
+    // OUT WHEN IT HAS REACHED A DESTINATION NODE
+    private boolean movingToTarget;
+    
+    public Bandit(SpriteType initSpriteType, float initX, float initY, float initVx, float initVy, 
+            String initState){
+        // SEND ALL THE Sprite DATA TO A Sprite CONSTRUCTOR
+        super(initSpriteType, initX, initY, initVx, initVy, initState);
+        
+        Node = 0;
     }
     
     public int getNode(){

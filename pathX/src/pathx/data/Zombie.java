488eef94737d3fc1d31bb198cbf08f74c3562ad2
Jessica <jessicazengg@gmail.com>
2014-05-02 21:16:45 -0400

    added constructor for sprite

--- pathX/src/pathx/data/Zombie.java
+++ pathX/src/pathx/data/Zombie.java
@@ -1,14 +1,33 @@
 package pathx.data;
 
+import mini_game.Sprite;
+import mini_game.SpriteType;
+
 /**
  *
  * @author Jessica
  */
-public class Zombie {
+public class Zombie extends Sprite{
+    int ID;
+    
+    // the current node this sprite is in
     int Node;
     
-    public Zombie(int newNode){
-        Node = newNode;
+    int targetX;
+    int targetY;
+    
+    int startX;
+    int startY;
+    
+    // THIS IS true WHEN THIS TILE IS MOVING, WHICH HELPS US FIGURE
+    // OUT WHEN IT HAS REACHED A DESTINATION NODE
+    private boolean movingToTarget;
+    
+    public Zombie(SpriteType initSpriteType, float initX, float initY, float initVx, float initVy, 
+            String initState){
+        super(initSpriteType, initX, initY, initVx, initVy, initState);
+        
+        Node = 0;
     }
     
     public int getNode(){

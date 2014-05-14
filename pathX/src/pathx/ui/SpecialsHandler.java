97f2611117715a25e0ff7f760063ce5304f4afac
Jessica <jessicazengg@gmail.com>
2014-05-14 16:59:15 -0400

    added freeze handler

---
+++ pathX/src/pathx/ui/SpecialsHandler.java
@@ -0,0 +1,42 @@
+package pathx.ui;
+
+import pathx.data.pathXDataModel;
+import static pathx.pathXConstants.*;
+
+/**
+ *
+ * @author Jessica
+ */
+public class SpecialsHandler {
+    private pathXGame game;
+    
+    public SpecialsHandler(pathXGame initGame)
+    {
+        game = initGame;
+    }
+    public void makeLightRed(){
+        // GET THE GAME'S DATA MODEL, WHICH IS ALREADY LOCKED FOR US
+        pathXDataModel data = (pathXDataModel)game.getDataModel();
+        
+        data.setSpecialSelected(true);
+        
+        data.setSpecial(MAKE_RED_LIGHT_BUTTON_TYPE);
+    }
+    
+    public void makeLightGreen(){
+        // GET THE GAME'S DATA MODEL, WHICH IS ALREADY LOCKED FOR US
+        pathXDataModel data = (pathXDataModel)game.getDataModel();
+        
+        data.setSpecialSelected(true);
+        
+        data.setSpecial(MAKE_GREEN_LIGHT_BUTTON_TYPE);
+    }
+    
+    public void freeze(){
+        // GET THE GAME'S DATA MODEL, WHICH IS ALREADY LOCKED FOR US
+        pathXDataModel data = (pathXDataModel)game.getDataModel();
+        
+        data.freeze();
+        data.setSpecial(FREEZE_UNFREEZE_BUTTON_TYPE);
+    }
+}
